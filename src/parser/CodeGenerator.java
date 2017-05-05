7705b8f9344c47d320bade04b4d9584dc8ac6557
Cat <catramos96@gmail.com>
2017-05-05 21:38:14 +0100

    Fixs

--- src/parser/CodeGenerator.java
+++ src/parser/CodeGenerator.java
@@ -2,6 +2,7 @@ package parser;
 
 import java.util.ArrayList;
 import cli.Resources;
+import cli.Resources.JSONType;
 import jdk.management.resource.ResourceAccuracy;
 
 public class CodeGenerator {
@@ -92,7 +93,7 @@ public class CodeGenerator {
 					code+= ")\n" + spacement + "{";
 					spacement += Resources.DEF_SPC;				//add 1 tab
 				}
-				code+="\n" + spacement + generate(c);
+				code+="\n" + spacement + generate(c) + ";";
 			}
 		}
 		spacement.replaceFirst(Resources.DEF_SPC, "");			//rem 1 tab
@@ -103,29 +104,28 @@ public class CodeGenerator {
 	private String handleVariableDeclaration(Node node){
 		boolean firstDeclaration = true;
 		//tmp ate resolver varias declaracoes por linha
-		String code = new String(node.getReference().type + " " + node.getReference().name); 	
+		String code = new String();//node.getReference().type + " " + node.getReference().name); 	
 		
 		for(Node n : node.getAdj()){
-			if(firstDeclaration) firstDeclaration = false;
+			if(firstDeclaration){ 
+				firstDeclaration = false;
+				code += n.getType() + " ";
+			}
 			else code += ", ";
-			code += /*n.getReference().name + */" = " + generate(n);
+			code += generate(n);
 		}
-		code += ";";
 		return code;
 	}
 	
 	private String handleAssignment(Node node, ArrayList<Node> assignment){
 		String code = new String("");
-		boolean left = true, multipleAssign = false;
+		boolean left = true;
 		
 		for(Node n : assignment){
 			if(left) left = false;
 			else code += " = ";
-			
 			code += generate(n);
-			if(n.getType() == Resources.JSONType.ASSIGNMENT) multipleAssign = true;
 		}
-		if(!multipleAssign) code+=";";
 		return code;
 	}
 	
@@ -147,4 +147,11 @@ public class CodeGenerator {
 		return code;
 	}
 
+	public JSONType getType(Node n){
+		if(n.getReference() == null){
+			return getType(n.getAdj().get(0));
+		}
+		else
+			return n.getType();
+	}
 }
