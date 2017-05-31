9078ef8aecf9ec742332dbfdf559924f3591695c
Cat <catramos96@gmail.com>
2017-05-31 13:09:40 +0100

    Revert "Fixs"
    This reverts commit 7705b8f9344c47d320bade04b4d9584dc8ac6557.

--- src/parser/CodeGenerator.java
+++ src/parser/CodeGenerator.java
@@ -2,8 +2,11 @@ package parser;
 
 import java.util.ArrayList;
 import cli.Resources;
+<<<<<<< HEAD
 import cli.Resources.DataType;
 import cli.Resources.JSONType;
+=======
+>>>>>>> parent of 7705b8f... Fixs
 import jdk.management.resource.ResourceAccuracy;
 
 public class CodeGenerator {
@@ -98,7 +101,7 @@ public class CodeGenerator {
 					code+= ")\n" + spacement + "{";
 					spacement += Resources.DEF_SPC;				//add 1 tab
 				}
-				code+="\n" + spacement + generate(c) + ";";
+				code+="\n" + spacement + generate(c);
 			}
 		}
 		spacement.replaceFirst(Resources.DEF_SPC, "");			//rem 1 tab
@@ -109,28 +112,36 @@ public class CodeGenerator {
 	private String handleVariableDeclaration(Node node){
 		boolean firstDeclaration = true;
 		//tmp ate resolver varias declaracoes por linha
-		String code = new String();//node.getReference().type + " " + node.getReference().name); 	
+		String code = new String(node.getReference().type + " " + node.getReference().name); 	
 		
 		for(Node n : node.getAdj()){
+<<<<<<< HEAD
 			if(firstDeclaration){ 
 				firstDeclaration = false;
 				code += getType(n) + " ";
 			}
+=======
+			if(firstDeclaration) firstDeclaration = false;
+>>>>>>> parent of 7705b8f... Fixs
 			else code += ", ";
-			code += generate(n);
+			code += /*n.getReference().name + */" = " + generate(n);
 		}
+		code += ";";
 		return code;
 	}
 	
 	private String handleAssignment(Node node, ArrayList<Node> assignment){
 		String code = new String("");
-		boolean left = true;
+		boolean left = true, multipleAssign = false;
 		
 		for(Node n : assignment){
 			if(left) left = false;
 			else code += " = ";
+			
 			code += generate(n);
+			if(n.getType() == Resources.JSONType.ASSIGNMENT) multipleAssign = true;
 		}
+		if(!multipleAssign) code+=";";
 		return code;
 	}
 	
@@ -165,6 +176,7 @@ public class CodeGenerator {
 		return code;
 	}
 
+<<<<<<< HEAD
 	public DataType getType(Node n){
 		if(n.getReference() == null){
 			return getType(n.getAdj().get(0));
@@ -172,4 +184,6 @@ public class CodeGenerator {
 		else
 			return n.getReference().type;
 	}
+=======
+>>>>>>> parent of 7705b8f... Fixs
 }
